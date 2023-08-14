9912a6eea3cbb76264dad8d40406cb4d360b93e1
Samantha Tobias <SamanthaArnoldussen@gmail.com>
2023-08-14 16:02:24 +0200

    initial commit

---
+++ src/main/java/io/samanthatobias/planetdiary/PlanetDiaryApplication.java
@@ -0,0 +1,13 @@
+package io.samanthatobias.planetdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetDiaryApplication.class, args);
+	}
+
+}
